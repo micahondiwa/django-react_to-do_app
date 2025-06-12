5ba49db8d8fe7acec4cf0a8430f8155873b615bc
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-12 10:26:41 +0300

    Models

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-from .models import Todo
+from .models import Todo 
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
 # Register your models here.
+
