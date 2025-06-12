49687d01d24d97aa591d9a22382c45f5c48d41f1
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-12 10:08:37 +0300

    Defining the data model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
