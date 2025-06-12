ea5dca4ea9ef8d84d78136103c1d455d6fe1d768
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-12 10:09:18 +0300

    Str repreentation

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -6,3 +6,6 @@ class Todo(models.model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
